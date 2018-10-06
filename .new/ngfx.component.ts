c5b5b18ca3b8a44372c23dbe8b540c8fc474caf1
Steve Belovarich <steveblue@gmail.com>
2018-10-06 14:51:55 -0700

    UPDATE default component

--- .new/ngfx.component.ts
+++ .new/ngfx.component.ts
@@ -1,7 +1,6 @@
 import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { NgFxControl } from './../../interfaces/control';
 import { NgFxController } from './../../services/controller/controller.service';
-import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'ngfx-new, [ngfx-new]',
@@ -12,30 +11,11 @@ export class NgFxComponent implements OnInit {
   @Input('control')
   control: NgFxControl;
 
-  @HostBinding('style.transform')
-  get transform(): SafeStyle {
-    return this.sanitize(this.control.transform) || this.sanitize('');
-  }
-
-  @HostBinding('style.grid-area')
-  get gridArea(): SafeStyle {
-    return this.sanitize(this.control.gridArea) || this.sanitize('');
-  }
-
-  @HostBinding('style.place-self')
-  get placeSelf(): SafeStyle {
-    return this.sanitize(this.control.placeSelf) || this.sanitize('');
-  }
-
-  constructor(private _controller: NgFxController, private _sanitizer: DomSanitizer) {}
+  constructor(private _controller: NgFxController) {}
 
   ngOnInit() {}
 
   hasName() {
     return this.control.name !== undefined && this.control.name.length > 0;
-  }
-
-  sanitize(style: string): SafeStyle {
-    return this._sanitizer.bypassSecurityTrustStyle(style);
   }
 }
